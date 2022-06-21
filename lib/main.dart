ea6173b3faa9d0a4e4a7bf3f20a10e0cc3f01c26
ThanitBurana <thanit_bur@freewillsolutions.com>
2022-06-21 09:48:12 +0700

    bloc login

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'module/home/screen/homescreen/homescreen.dart';
-import 'module/login/bloc/loginbloc/login_bloc.dart';
-import 'module/login/screen/forgotpasswordscreen/setnewforgotpassword.dart';
-import 'module/main/mainbloc/main_bloc.dart';
-import 'package:f2fbuu/module/login/screen/loginscreen/loginscreen.dart';
+
 import 'package:flutter/services.dart';
+
+import 'module/login/bloc/loginbloc/login_bloc.dart';
 
 void main() async {
 
@@ -23,31 +22,36 @@ void main() async {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // primarySwatch: Colors.blue,
-        scaffoldBackgroundColor: Colors.white,
-      ),
-      home: HomeScreen(),
-      // home: ProfileScreen(),
+    final loginBloc = BlocProvider<LoginBloc>(create: (context)=> LoginBloc());
+    return MultiBlocProvider(
+      providers: [loginBloc],
+      child: MaterialApp(
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            // primarySwatch: Colors.blue,
+            scaffoldBackgroundColor: Colors.white,
+          ),
+          home: HomeScreen(),
+          // home: ProfileScreen(),
 ///////////////////////
 
-      // final loginBloc = BlocProvider<LoginBloc>(create: (context)=> LoginBloc());
-      // final MainnBloc = BlocProvider<MainBloc>(create: (context)=> MainBloc());
-      // return MultiBlocProvider(
-      //     providers: [loginBloc,MainnBloc],
-      //     child: MaterialApp
-      //       (
-      //       title: 'Flutter Demo',
-      //       theme: ThemeData(
-      //         primarySwatch: Colors.grey,
-      //       ),
-      //       home: ProfileScreen(),
-      //     )
+          // final loginBloc = BlocProvider<LoginBloc>(create: (context)=> LoginBloc());
+          // final MainnBloc = BlocProvider<MainBloc>(create: (context)=> MainBloc());
+          // return MultiBlocProvider(
+          //     providers: [loginBloc,MainnBloc],
+          //     child: MaterialApp
+          //       (
+          //       title: 'Flutter Demo',
+          //       theme: ThemeData(
+          //         primarySwatch: Colors.grey,
+          //       ),
+          //       home: ProfileScreen(),
+          //     )
+        ),
     );
   }
 }
---
+++ lib/module/login/repository/login_repository.dart
@@ -0,0 +1,8 @@
+import 'package:dio/src/response.dart';
+import 'package:f2fbuu/utils/dio.dart';
+
+class LoginRepository  {
+  Future<Response> getScreenLogin() async {
+    return await MyDio.createDio().post("/v1/api/modules/login/wording/login");
+  }
+}
