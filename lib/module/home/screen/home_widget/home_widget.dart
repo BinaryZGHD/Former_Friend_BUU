c2c585c0aea969d2f3a0009c7400ee9fd1bd2d2a
IZBINARYZI <oonniihhcc@gmail.com>
2022-07-22 13:15:33 +0700

    SafeArea Home

--- lib/module/home/screen/home_widget/home_widget.dart
+++ lib/module/home/screen/home_widget/home_widget.dart
@@ -52,121 +52,123 @@ buildContentHomeScreen(
           ),
         ],
       ),
-      body: Column(
-        children: [
-          const SizedBox(
-            height: 5,
-          ),
-          if (screenstatusActivityResponse?.body?.activity?.length.toInt() == 0)
-            Expanded(
-              child: Card(
-                color: Colors.grey[200],
-                child: Container(
-                    decoration: BoxDecoration(borderRadius: BorderRadius.circular(20), color: transparent),
-                    width: MediaQuery.of(context).size.width,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children:  [
-                        const Icon(
-                          Icons.running_with_errors_outlined,
-                          color: tcNoActivity,
-                          size: 100,
-                        ),
-                        const SizedBox(
-                          height: 10,
-                        ),
-                        Text("${alertNoActivityResponse?.body?.noactiviry}",
-                            style: const TextStyle(fontSize: 20, fontWeight: FontWeight.w500, color: tcNoActivity)),
-                        const SizedBox(
-                          height: 5,
-                        ),
-                        Text("${alertNoActivityResponse?.body?.subactlineone}",
-                            style: const TextStyle(fontSize: 15, fontWeight: FontWeight.w500, color: tcNoActivity)),
-                        const SizedBox(
-                          height: 5,
-                        ),
-                        Text("${alertNoActivityResponse?.body?.subactlinetwo}",
-                            style: const TextStyle(fontSize: 15, fontWeight: FontWeight.w500, color: tcNoActivity)),
-                      ],
-                    )),
-              ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 5,
             ),
-          if (screenstatusActivityResponse?.body?.activity?.length.toInt() != 0)
-            Expanded(
-              child: Container(
-                color: tcButtonTextWhite,
-                child: Padding(
-                  padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
-                  child: SingleChildScrollView(
-                    child: Column(
-                      children: [
-                        buildListActivity(context, screenstatusActivityResponse),
-                        const SizedBox(
+            if (screenstatusActivityResponse?.body?.activity?.length.toInt() == 0)
+              Expanded(
+                child: Card(
+                  color: Colors.grey[200],
+                  child: Container(
+                      decoration: BoxDecoration(borderRadius: BorderRadius.circular(20), color: transparent),
+                      width: MediaQuery.of(context).size.width,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children:  [
+                          const Icon(
+                            Icons.running_with_errors_outlined,
+                            color: tcNoActivity,
+                            size: 100,
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          Text("${alertNoActivityResponse?.body?.noactiviry}",
+                              style: const TextStyle(fontSize: 20, fontWeight: FontWeight.w500, color: tcNoActivity)),
+                          const SizedBox(
+                            height: 5,
+                          ),
+                          Text("${alertNoActivityResponse?.body?.subactlineone}",
+                              style: const TextStyle(fontSize: 15, fontWeight: FontWeight.w500, color: tcNoActivity)),
+                          const SizedBox(
+                            height: 5,
+                          ),
+                          Text("${alertNoActivityResponse?.body?.subactlinetwo}",
+                              style: const TextStyle(fontSize: 15, fontWeight: FontWeight.w500, color: tcNoActivity)),
+                        ],
+                      )),
+                ),
+              ),
+            if (screenstatusActivityResponse?.body?.activity?.length.toInt() != 0)
+              Expanded(
+                child: Container(
+                  color: tcButtonTextWhite,
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+                    child: SingleChildScrollView(
+                      child: Column(
+                        children: [
+                          buildListActivity(context, screenstatusActivityResponse),
+                          const SizedBox(
                             // height: MediaQuery.of(context).size.height * 0.2,
-                            ),
-                      ],
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ),
               ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.01,
             ),
-          SizedBox(
-            height: MediaQuery.of(context).size.height * 0.01,
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(0, 3, 0, 0),
-            child: ButtonCustom(
-              label: "       ${screenhomeResponse?.body?.screenInfo?.btnadd}      ",
-              colortext: tcButtonTextBlack,
-              colorbutton: tcButtonTextWhite,
-              sizetext: sizeTextSmaller14,
-              colorborder: tcButtonTextBoarder,
-              sizeborder: 1.0,
-              onPressed: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) => const AddActivityScreen()));
-              },
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(0, 0, 0, 10),
-
-            // padding: const EdgeInsets.only(bottom: 10.0),
-            child: Container(
-              color: tcButtonTextWhite,
-              height: 50,
-              child: Row(
-                // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Expanded(
-                      child: IconButton(
-                    icon: const Icon(Icons.account_circle, color: Colors.black, size: 50),
-                    onPressed: () {
-                      Navigator.push(context, MaterialPageRoute(builder: (context) => const ProfileScreen()));
-                    },
-                  )),
-                  Expanded(
-                      child: IconButton(
-                    icon: const Icon(Icons.home, color: Colors.blue, size: 50),
-                    onPressed: () {},
-                  )),
-                  Expanded(
-                      child: IconButton(
-                    icon: const Icon(Icons.auto_awesome_mosaic, color: Colors.black, size: 50),
-                    onPressed: () {
-                      // Navigator.push(context, MaterialPageRoute(builder: (context) => ProfileScreen()));
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => const MoreScreen()));
-                    },
-                  )),
-                ],
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 3, 0, 0),
+              child: ButtonCustom(
+                label: "       ${screenhomeResponse?.body?.screenInfo?.btnadd}      ",
+                colortext: tcButtonTextBlack,
+                colorbutton: tcButtonTextWhite,
+                sizetext: sizeTextSmaller14,
+                colorborder: tcButtonTextBoarder,
+                sizeborder: 1.0,
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => const AddActivityScreen()));
+                },
               ),
             ),
-          ),
-        ],
-      ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 0, 0, 10),
+
+              // padding: const EdgeInsets.only(bottom: 10.0),
+              child: Container(
+                color: tcButtonTextWhite,
+                height: 50,
+                child: Row(
+                  // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Expanded(
+                        child: IconButton(
+                          icon: const Icon(Icons.account_circle, color: Colors.black, size: 50),
+                          onPressed: () {
+                            Navigator.push(context, MaterialPageRoute(builder: (context) => const ProfileScreen()));
+                          },
+                        )),
+                    Expanded(
+                        child: IconButton(
+                          icon: const Icon(Icons.home, color: Colors.blue, size: 50),
+                          onPressed: () {},
+                        )),
+                    Expanded(
+                        child: IconButton(
+                          icon: const Icon(Icons.auto_awesome_mosaic, color: Colors.black, size: 50),
+                          onPressed: () {
+                            // Navigator.push(context, MaterialPageRoute(builder: (context) => ProfileScreen()));
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => const MoreScreen()));
+                          },
+                        )),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      )
     ),
   );
 }
